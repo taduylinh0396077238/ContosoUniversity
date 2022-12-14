a2213901eff7a41f7727ad1a9d1c16c03ae61a53
Duy Linh <91305016+taduylinh0396077238@users.noreply.github.com>
2022-12-14 22:45:23 +0700

    Initial commit

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+//using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly StudentsController _context;
+        public HomeController(ILogger<HomeController> logger, StudentsController context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ContosoUniversity/Data/ContosoUniversityContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data
+{
+    public class ContosoUniversity : DbContext
+    {
+        public ContosoUniversity(DbContextOptions<ContosoUniversity> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; } = default!;
+        public DbSet<ContosoUniversity.Models.Course> Course { get; set; } = default!;
+        public DbSet<ContosoUniversity.Models.Enrollment> Enrollment { get; set; } = default!;
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+
+    }
+}
